a97da02c11a4aa910484956dac7a1389a13f05a4
smsohan <sohan39@gmail.com>
2020-04-12 09:49:52 -0600

    adds a logger

--- lib/actionhook.rb
+++ lib/actionhook.rb
@@ -1,5 +1,6 @@
-require "actionhook/version"
+require 'logger'
 require "ipaddr"
+require "actionhook/version"
 require "actionhook/core/configuration"
 require "actionhook/security/authentication"
 require "actionhook/core/request"
@@ -8,7 +9,16 @@ require "actionhook/core/net_http_sender"
 
 module ActionHook
   class Error < StandardError; end
+
   DEFAULT_CONFIGURATION = ActionHook::Core::Configuration.new
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
+  def self.logger
+    @logger ||= Logger.new(STDOUT)
+  end
 
   def self.configuration=(configuration)
     @configuration = configuration
--- spec/actionhook_spec.rb
+++ spec/actionhook_spec.rb
@@ -12,4 +12,14 @@ describe ActionHook do
     expect(ActionHook.configuration).to eql(custom_config)
   end
 
+  it 'has a logger' do
+    expect(ActionHook.logger).not_to be_nil
+  end
+
+  it 'allows a custom logger to be set' do
+    logger = Logger.new("/dev/null")
+    ActionHook.logger = logger
+    expect(ActionHook.logger).to eql logger
+  end
+
 end

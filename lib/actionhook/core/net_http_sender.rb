8dc0f0f1f113f4b88ee4d797b5ea432bf25a5b40
smsohan <sohan39@gmail.com>
2020-04-12 09:59:29 -0600

    adds logging

--- lib/actionhook/core/net_http_sender.rb
+++ lib/actionhook/core/net_http_sender.rb
@@ -21,14 +21,23 @@ module ActionHook
       end
 
       def self.send(request, configuration = ActionHook.configuration)
-        verify_allowed!(configuration, request.uri.host)
-        options = { use_ssl: request.uri.scheme == 'https' }.merge(configuration.net_http_options)
+        ActionHook.logger.info "ActionHook: Send called for #{request.method.upcase} to #{request.uri}"
 
+        ActionHook.logger.debug "ActonHook: Using configuration: #{configuration.inspect}"
+        ActionHook.logger.debug "ActonHook: using net/http options #{configuration.net_http_options}"
+
+        verify_allowed!(configuration, request.uri.host)
+        ActionHook.logger.debug "ActonHook: #{request.uri.host} is clear, not blocked"
+
+        options = { use_ssl: request.uri.scheme == 'https' }.merge(configuration.net_http_options)
         Net::HTTP.start(request.uri.host, request.uri.port, options) do |http|
           http_request = request_method_class(request.method).new request.uri
           http_request.body = request.serialized_body if request.body
 
+          ActionHook.logger.debug "ActonHook: Body: #{http_request.body}"
+
           request.headers_with_security(configuration)&.each_pair do |name, value|
+            ActionHook.logger.debug "ActonHook: Added Security Header: #{name}"
             http_request[name] = value.to_s
           end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 require 'actionhook'
 require 'webmock/rspec'
 
+ActionHook.logger = Logger.new("/dev/null")
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
